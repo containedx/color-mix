aaab68ab8fcee7531382bf90b0b9cbfcda1c6ef7
Kinga Zawarty <niu.ula.a@gmail.com>
2023-09-08 15:07:55 +0200

    setting up simple UI for color mixer

--- ColorMix/Assets/MixTest.cs
+++ ColorMix/Assets/MixTest.cs
@@ -13,9 +13,9 @@ public class MixTest : MonoBehaviour
     void Start()
     {
 
-	var red = new Color(255, 0, 0, 100);
-	var blue = new Color(0, 0, 255, 100);
-	var green = new Color(0, 255, 0, 100);
+	var red = new Color(255, 0, 0);
+	var blue = new Color(0, 0, 255);
+	var green = new Color(0, 255, 0);
 
 
 	img.color = green + red;
